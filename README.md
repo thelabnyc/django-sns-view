3c64eb1a681ff66ff540f81c964452cb6d4fc31a
Craig Weber <crgwbr@gmail.com>
2025-03-12 11:39:52 -0400

    feat: fork [django-sns-view](https://github.com/deep-c/django-sns-view) to begin bringing it up to date

---
+++ README.md
@@ -0,0 +1,41 @@
+# Django SNS View
+
+A drop in configurable django view that is used to subscribe and process AWS SNS messages. 
+
+This repo is a fork of the (now unmaintained) [django-sns-view](https://pypi.org/project/django-sns-view/) package (see also [deep-c/django-sns-view](https://github.com/deep-c/django-sns-view) on GitHub).
+
+## Installation
+
+```sh
+pip install django-sns-view
+```
+
+## Default Django Settings
+
+```py
+SNS_CERT_DOMAIN_REGEX = r"sns.[a-z0-9\-]+.amazonaws.com$" # Regex to match on cert domain
+SNS_VERIFY_CERTIFICATE = True # Whether to verify signature against certificate
+```
+
+## SNSEndpoint Attributes
+
+```py
+message_type_header = 'HTTP_X_AMZ_SNS_MESSAGE_TYPE'
+topic_type_header = 'HTTP_X_AMZ_SNS_TOPIC_ARN'
+allowed_message_types = ['Notification', 'SubscriptionConfirmation', 'UnsubscribeConfirmation']
+cert_domain_settings_key = 'SNS_CERT_DOMAIN_REGEX'
+sns_verify_settings_key = 'SNS_VERIFY_CERTIFICATE'
+topic_settings_key = '' # If you would like to subscribe this endpoint only certain topics, create a setting containing a list of topics that are allowed.
+```
+
+### Usage
+
+```py
+from django_sns_view.views import SNSEndpoint
+
+class MySNSView(SNSEndpoint):
+    # Can override SNSEndpoint attributes outlined above
+
+    def handle_message(self, message, payload):
+        # Process the message
+```
--- django_sns_view/tests/test_settings.py
+++ django_sns_view/tests/test_settings.py
@@ -1,73 +1,55 @@
-"""Settings that need to be set in order to run the tests."""
 import os
 
-
+SECRET_KEY = "foobar"
 DEBUG = True
 SITE_ID = 1
 
-APP_ROOT = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..'))
+APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
+    "django.contrib.sites",
+    "django_sns_view",
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
     }
 }
 
-ROOT_URLCONF = 'django_sns_view.tests.urls'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(APP_ROOT, '../app_static')
-MEDIA_ROOT = os.path.join(APP_ROOT, '../app_media')
-STATICFILES_DIRS = (
-    os.path.join(APP_ROOT, 'static'),
-)
-
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'APP_DIRS': True,
-    'DIRS': [os.path.join(APP_ROOT, 'tests/test_app/templates')],
-    'OPTIONS': {
-        'context_processors': (
-            'django.contrib.auth.context_processors.auth',
-            'django.template.context_processors.request',
-        )
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": (
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
+            )
+        },
     }
-}]
-
-EXTERNAL_APPS = [
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'django.contrib.sitemaps',
-    'django.contrib.sites',
-]
-
-INTERNAL_APPS = [
-    'django_sns_view',
-]
-
-INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
-
-MIDDLEWARE_CLASSES = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-]
-
-SECRET_KEY = 'foobar'
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-    '--with-xunit',
-    '--nologcapture',
-    '--cover-package=django_sns_view',
 ]
